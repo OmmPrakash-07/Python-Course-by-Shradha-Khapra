354750e1ec599ecfad937e987d84756d02589860
Omm Prakash Parida <ommprakashparida114@gmail.com>
2026-07-13 23:45:03 +0530

    New

--- Input_In_Python.py
+++ Input_In_Python.py
@@ -5,3 +5,12 @@ print("Welcome", name)
 
 val = input("enter some value: ")
 print(type(val), val) # Convert to string
+
+
+name = input("Enter Name: ")
+age = input("Enter Age: ")
+mark = input("Enter Mark: ")
+
+print("Welcome", name)
+print("Age", age)
+print("Mark", mark)

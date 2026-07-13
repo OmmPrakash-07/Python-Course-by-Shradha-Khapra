a619c679f09ff043eb2989814de26ae2476bb2d6
Omm Prakash Parida <ommprakashparida114@gmail.com>
2026-07-13 21:06:44 +0530

    New

--- operators.py
+++ operators.py
@@ -15,7 +15,9 @@ print(a ** b) #a^b
 a = 50
 b = 20
 
-print(a == b) #False
+print (a == b) #False
 print (a != b) #True
 print (a >= b) #True
 print (a > b) #True
+print (a <= b) #False
+print (a < b) #False

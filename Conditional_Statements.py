8e2c3024d858f0c3864f858e4b757368c6ed66b5
Omm Prakash Parida <ommprakashparida114@gmail.com>
2026-07-29 20:58:02 +0530

    New

--- Conditional_Statements.py
+++ Conditional_Statements.py
@@ -2,6 +2,7 @@ age = 21
 if (age>=18):
     print("can vote and apply for license")
 
+
 light ="green"
 if (light == "red"):
     print("Stop")
@@ -9,3 +10,21 @@ elif (light == "green"):
     print("Go")
 elif(light == "yellow"):
     print("Wait")
+
+
+light ="pink"
+if (light == "red"):
+    print("Stop")
+elif (light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("Wait")
+else:
+    print("Light is Broken ")
+
+
+age = 21
+if (age>=18):
+    print("can vote")
+else:
+    print("can't vote")
